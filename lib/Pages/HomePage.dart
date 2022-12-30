885bd55c3a3a874e4f0be7bad5cabc3c3ebbf2f7
RISHABH KUMAR <rishabhkumar7389@gmail.com>
2022-12-31 00:26:18 +0530

    text change

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -25,7 +25,7 @@ class HomePage extends StatelessWidget {
                 height: 10,
               ),
               Text(
-                "'Enter Your Details'",
+                "Welcome",
                 style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
               ),
               SizedBox(
