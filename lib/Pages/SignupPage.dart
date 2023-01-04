b1868bde1c58b729cdf05660cbdc7943229f8a8d
RISHABH KUMAR <rishabhkumar7389@gmail.com>
2023-01-04 11:03:30 +0530

    .

--- lib/Pages/SignupPage.dart
+++ lib/Pages/SignupPage.dart
@@ -9,36 +9,46 @@ class SignupPage extends StatelessWidget {
     return Material(
       child: Column(
         children: [
-          Text("Sign Up"),
+          SizedBox(height: 70),
+          Text(
+            "Sign Up",
+            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+          ),
           SizedBox(
             height: 10,
           ),
           Text("Create an Accont,its free"),
           SizedBox(
-            height: 10,
+            height: 40,
           ),
           Container(
             padding: const EdgeInsets.all(10),
             child: TextFormField(
               decoration: const InputDecoration(
-                  border: OutlineInputBorder(), labelText: "Name"),
+                  border: OutlineInputBorder(), labelText: "Email"),
             ),
           ),
           Container(
             padding: const EdgeInsets.all(10),
             child: TextFormField(
+              obscureText: true,
               decoration: const InputDecoration(
-                  border: OutlineInputBorder(), labelText: "Registration No."),
+                  border: OutlineInputBorder(), labelText: "Password"),
             ),
           ),
           Container(
               padding: const EdgeInsets.all(10),
               child: TextFormField(
+                obscureText: true,
                 decoration: const InputDecoration(
-                    border: OutlineInputBorder(), labelText: "Hostel Name"),
+                    border: OutlineInputBorder(),
+                    labelText: "Confirm Password"),
               )),
+          SizedBox(
+            height: 10,
+          ),
           Container(
-              height: 40,
+              height: 50,
               width: 300,
               child: ElevatedButton(
                   onPressed: () {
