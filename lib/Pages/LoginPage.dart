5fea665532e9e9cb9b2feeb329cd25bab806c298
RISHABH KUMAR <rishabhkumar7389@gmail.com>
2023-01-10 10:43:57 +0530

    text added in firebase

--- lib/Pages/LoginPage.dart
+++ lib/Pages/LoginPage.dart
@@ -38,6 +38,8 @@ class _LoginPageState extends State<LoginPage> {
           context,
           MaterialPageRoute(builder: (context) => HomePage()),
         );
+      }).onError((error, stackTrace) {
+        print("error ${toString()}");
       });
     }
   }
