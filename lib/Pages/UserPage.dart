55a8e2a24ad5a1d3b8afd3552b51893abad87047
RISHABH KUMAR <rishabhkumar7389@gmail.com>
2023-01-11 22:14:35 +0530

    login box added

--- lib/Pages/UserPage.dart
+++ lib/Pages/UserPage.dart
@@ -7,11 +7,14 @@ class UserPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.purple,
         title: Center(
-            child: const Text(
-          "UserType",
-          style: TextStyle(color: Colors.white),
-        )),
+          child: const Text(
+            "UserType",
+            style: TextStyle(
+                color: Colors.white, fontSize: 23, fontWeight: FontWeight.bold),
+          ),
+        ),
       ),
       body: Material(
         child: Column(
@@ -19,17 +22,73 @@ class UserPage extends StatelessWidget {
             SizedBox(
               height: 30,
             ),
-            Text(
-              "Select User Type",
-              style: TextStyle(
-                  color: Colors.black,
+            Container(
+              child: Text(
+                "Select User Type",
+                style: TextStyle(
+                  color: Colors.red,
                   fontWeight: FontWeight.bold,
-                  fontSize: 30),
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              child: Text(
+                "Student Login",
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.purple),
+              ),
+              width: 350,
+              height: 130,
+              decoration: BoxDecoration(
+                border: Border.all(
+                    color: Color.fromARGB(255, 3, 225, 237), width: 2),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              child: Text(
+                "Admin Login",
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.purple),
+              ),
+              width: 350,
+              height: 130,
+              decoration: BoxDecoration(
+                border: Border.all(
+                    color: Color.fromARGB(255, 3, 225, 237), width: 2),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              child: Text(
+                "Guest Login",
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.purple),
+              ),
+              width: 350,
+              height: 130,
+              decoration: BoxDecoration(
+                border: Border.all(
+                    color: Color.fromARGB(255, 3, 225, 237), width: 2),
+              ),
             ),
           ],
         ),
       ),
-      drawer: Drawer(),
     );
   }
 }
