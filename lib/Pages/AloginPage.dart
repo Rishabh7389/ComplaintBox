d60739879b50446ac53d8187333b7c7b0441521b
RISHABH KUMAR <rishabhkumar7389@gmail.com>
2023-01-12 17:21:42 +0530

    textformfield added

--- lib/Pages/AloginPage.dart
+++ lib/Pages/AloginPage.dart
@@ -11,6 +11,42 @@ class ALoginPage extends StatelessWidget {
           Container(
             child: Image.asset("assets/images/complain.jpg"),
           ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "WELCOME",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "Admin",
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: TextFormField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: "Email")),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Password",
+              ),
+            ),
+          ),
         ],
       ),
     );
