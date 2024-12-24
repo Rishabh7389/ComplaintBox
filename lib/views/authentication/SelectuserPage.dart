b035a89e7a28a470fe0ad44ba20d3ccb29891109
Rishabh Kumar <rishabh@Rishabhs-MacBook-Air.local>
2024-12-24 17:04:24 +0530

    animation added to SelectUserPage

--- lib/views/authentication/SelectuserPage.dart
+++ lib/views/authentication/SelectuserPage.dart
@@ -4,8 +4,30 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/views/authentication/admin/LoginPageAdmin.dart';
 import 'package:flutter_application_1/views/authentication/student/LoginPage.dart';
 
-class UserPage extends StatelessWidget {
+class UserPage extends StatefulWidget {
   const UserPage({super.key});
+
+  @override
+  State<UserPage> createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> {
+  bool isStudentLoading = false;
+  bool isAdminLoading = false;
+
+  Future<void> navigateWithLoading(
+      BuildContext context, Widget targetPage, Function setLoading) async {
+    setLoading(true);
+
+    await Future.delayed(const Duration(milliseconds: 1500));
+
+    setLoading(false);
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => targetPage),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,6 +68,10 @@ class UserPage extends StatelessWidget {
                     "Complaint Box For Student",
                     "With Responsive and Fresh Look and Feel",
                     const LoginPage(),
+                    isStudentLoading,
+                    (value) => setState(() {
+                      isStudentLoading = value;
+                    }),
                   ),
                   SizedBox(height: screenHeight * 0.03),
                   buildUserCard(
@@ -53,6 +79,10 @@ class UserPage extends StatelessWidget {
                     "Complaint Box For Admin",
                     "With more information about Complaint",
                     const LoginPage2(),
+                    isAdminLoading,
+                    (value) => setState(() {
+                      isAdminLoading = value;
+                    }),
                   ),
                 ],
               ),
@@ -64,7 +94,7 @@ class UserPage extends StatelessWidget {
   }
 
   Widget buildUserCard(BuildContext context, String title, String description,
-      Widget targetPage) {
+      Widget targetPage, bool isLoading, Function(bool) setLoading) {
     final screenWidth = MediaQuery.of(context).size.width;
 
     return Container(
@@ -98,18 +128,25 @@ class UserPage extends StatelessWidget {
           Align(
             alignment: Alignment.centerRight,
             child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => targetPage),
-                );
-              },
+              onPressed: isLoading
+                  ? null // Disable the button while loading
+                  : () => navigateWithLoading(context, targetPage, setLoading),
               style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xff2b3258)),
-              child: const Text(
-                'Login',
-                style: TextStyle(fontSize: 18, color: Colors.white),
+                backgroundColor: const Color(0xff2b3258),
               ),
+              child: isLoading
+                  ? const SizedBox(
+                      width: 20,
+                      height: 20,
+                      child: CircularProgressIndicator(
+                        color: Colors.white,
+                        strokeWidth: 2,
+                      ),
+                    )
+                  : const Text(
+                      'Login',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
             ),
           ),
         ],
